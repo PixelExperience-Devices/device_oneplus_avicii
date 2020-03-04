6ce5bccbc8964c9a2bf29d3002ede714010691dc
Ashwin R C <ashwin2001achu@gmail.com>
2020-03-04 16:44:58 +0000

    avicii: init: Set Dalvik properties according to RAM size.
    Change-Id: I42310c967c495bd708f1ecae47f410dd33430b61

--- init/init_avicii.cpp
+++ init/init_avicii.cpp
@@ -1,5 +1,6 @@
 /*
    Copyright (c) 2020, The LineageOS Project
+                 2020, The Potato Open Sauce Project
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -32,6 +33,7 @@
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <stdio.h>
 #include <sys/_system_properties.h>
+#include <sys/sysinfo.h>
 #include <sys/system_properties.h>
 
 #include "property_service.h"
@@ -49,5 +51,40 @@ void property_override(char const prop[], char const value[]) {
     __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
+void load_12gb() {
+  property_override("dalvik.vm.heapstartsize", "24m");
+  property_override("dalvik.vm.heapgrowthlimit", "384m");
+  property_override("dalvik.vm.heaptargetutilization", "0.42");
+  property_override("dalvik.vm.heapmaxfree", "56m");
+}
+
+void load_8gb() {
+  property_override("dalvik.vm.heapstartsize", "24m");
+  property_override("dalvik.vm.heapgrowthlimit", "256m");
+  property_override("dalvik.vm.heaptargetutilization", "0.46");
+  property_override("dalvik.vm.heapmaxfree", "48m");
+}
+
+void load_6gb() {
+  property_override("dalvik.vm.heapstartsize", "16m");
+  property_override("dalvik.vm.heapgrowthlimit", "256m");
+  property_override("dalvik.vm.heaptargetutilization", "0.5");
+  property_override("dalvik.vm.heapmaxfree", "32m");
+}
+
+/* Check RAM size for current variant. */
+void checkram_loadprops() {
+  struct sysinfo sys;
+  sysinfo(&sys);
+  if (sys.totalram > 8192ull * 1024 * 1024) {
+    load_12gb();
+  } else if (sys.totalram > 6144ull * 1024 * 1024) {
+    load_8gb();
+  } else if (sys.totalram > 4096ull * 1024 * 1024) {
+    load_6gb();
+  }
+}
+
 void vendor_load_properties() {
+  checkram_loadprops();
 }
