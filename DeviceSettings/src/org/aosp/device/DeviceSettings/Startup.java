5f969ad96e44f87d44aca5cddcc97a92d76bcab7
itsjoeoui <idkwhoiam@itsjoeoui.com>
2020-05-31 09:09:46 -0400

    avicii: DeviceSettings: Remove vibrator control
    Signed-off-by: itsjoeoui <idkwhoiam@itsjoeoui.com>
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>

--- DeviceSettings/src/org/aosp/device/DeviceSettings/Startup.java
+++ DeviceSettings/src/org/aosp/device/DeviceSettings/Startup.java
@@ -30,10 +30,6 @@ public class Startup extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, final Intent bootintent) {
 
-        VibratorStrengthPreference.restore(context);
-        VibratorCallStrengthPreference.restore(context);
-        VibratorNotifStrengthPreference.restore(context);
-
         boolean enabled = false;
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
