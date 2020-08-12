ce35eef9bbc9f1e6e7cef58f69994699115f895b
chandra <chandra.chaganti@hotmail.com>
2020-08-12 11:38:47 +0200

    avicii: Switch back to bool for FOD
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -49,8 +49,7 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.telephony.ims.xml \
-    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml \
-    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
+    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/handheld_core_hardware.xml
 
 # A/B
 AB_OTA_UPDATER := true
