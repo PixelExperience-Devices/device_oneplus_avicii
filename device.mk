72e218fcb405226572e5da524d61a2e4109f3c64
CryllicBuster273 <cryllicbuster273@pixelexperience.org>
2020-08-21 12:23:50 +0530

    avicii: Update overlays from stock * move overlay-lineage to overlay-aosp
    Co-authored-by: iamsaalim <saalimquadri1@gmail.com>
    Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>

--- device.mk
+++ device.mk
@@ -26,7 +26,7 @@ $(call inherit-product, vendor/oneplus/avicii/avicii-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_PACKAGES += \
     OnePlusIconShapeCircleOverlay \
