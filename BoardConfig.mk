47b953a95c98f1e5c94e45ca5cb378ff6bad867e
LuK1337 <priv.luk@gmail.com>
2020-10-09 21:19:39 +0200

    avicii: Include QSSI sepolicy
    * Now that device/qcom/sepolicy is non-vendor only
      we are able to do that.
    
    Change-Id: I4c83a8e30cfa11e45be2f06eb7f444de7f68d219

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -154,16 +154,10 @@ BOARD_ROOT_EXTRA_FOLDERS := op1 op2
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # Sepolicy
+include device/qcom/sepolicy/SEPolicy.mk
+
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/qcom/sepolicy/generic/private \
-    device/qcom/sepolicy/qva/private
-
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/qcom/sepolicy/generic/public \
-    device/qcom/sepolicy/qva/public
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
