859b1d6d343a32d0907c3610a2c150eb547f32ad
Michael Bestas <mkbestas@lineageos.org>
2020-10-18 19:16:49 +0300

    avicii: Remove unused TARGET_SPECIFIC_HEADER_PATH
    Change-Id: I5518d31fa6c6f56f05dc374bfd8f98b3cd2e62cc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,8 +18,6 @@ BOARD_VENDOR := oneplus
 
 DEVICE_PATH := device/oneplus/avicii
 
-TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
-
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
