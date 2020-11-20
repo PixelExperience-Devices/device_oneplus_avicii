1fc733f3f196a764cdc78be73add313205556a04
LuK1337 <priv.luk@gmail.com>
2020-11-20 19:03:02 +0100

    avicii: Import and build QTI vibrator HAL
    * The one in vendor requires some special OnePlus FWK bits.
    
    Change-Id: I5d3be54b2aa3ab35d5a6f88b831ea6abbe8279c7

--- device.mk
+++ device.mk
@@ -228,6 +228,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service.oneplus_lito
+
 # WiFi
 PRODUCT_PACKAGES += \
     WifiResCommon
