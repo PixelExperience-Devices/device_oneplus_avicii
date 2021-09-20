e84aee7a9215eb6d3fc3f0255a167d0f3169127a
Bruno Martins <bgcngm@gmail.com>
2021-09-20 18:23:58 +0100

    avicii: Blob out ODM feature list
    There's not much of a purpose to have this in the device tree.
    
    Change-Id: I67751642b2f34576106a39a83e21996010215c3d

--- device.mk
+++ device.mk
@@ -46,10 +46,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     fstab.qcom
 
-# OPFeature
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/odm_feature_list:$(TARGET_COPY_OUT_ODM)/etc/odm_feature_list
-
 # tri-state-key
 PRODUCT_PACKAGES += \
     KeyHandler \
