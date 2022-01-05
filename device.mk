ecf1dc6c2148c8413be9bb80b0b73612478ba41c
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2022-01-05 20:15:37 +0530

    avicii: Nuke Google Camera

--- device.mk
+++ device.mk
@@ -22,9 +22,6 @@
 # definition file).
 #
 
-# Google Camera
-$(call inherit-product-if-exists, packages/apps/GoogleCamera/config.mk)
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
