ba2e4ad56051b56e2ce069f861ae43e0010fa84b
LuK1337 <priv.luk@gmail.com>
2022-07-09 18:25:22 +0200

    avicii: Override available touchscreen gestures list
    Change-Id: I14b0e034094c0cf4b60a86f5f26fc8d77ab50e43

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -173,6 +173,11 @@ VENDOR_SECURITY_PATCH := 2022-12-05
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
 
+# Touch
+SOONG_CONFIG_NAMESPACES += OPLUS_LINEAGE_TOUCH_HAL
+SOONG_CONFIG_OPLUS_LINEAGE_TOUCH_HAL := INCLUDE_DIR
+SOONG_CONFIG_OPLUS_LINEAGE_TOUCH_HAL_INCLUDE_DIR := $(DEVICE_PATH)/touch/include
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
