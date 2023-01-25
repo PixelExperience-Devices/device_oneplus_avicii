744525f2b6f617f4596262b10277e8bda2ae2f95
Vishalcj17 <vishalcj@aospa.co>
2023-01-25 18:52:33 +0530

    avicii: Generate thermal config from OOS.
     * Generated using the command
       "adb shell thermal-engine -o > thermal-engine.conf"
    
     * We don't use thermal daemon from OOS which has these
       changes inline, instead, make CAF thermal-engine happy
       with the config.
    
    Change-Id: I02e45dfbf9880c8e8418c4d8d9b4d827b4d7066d
     thirteen

--- device.mk
+++ device.mk
@@ -410,6 +410,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.oplus
