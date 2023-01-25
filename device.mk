09f0d009ca251f89bbcadc9fe6f38f28442cd67e
Alexander Koskovich <zvnexus@outlook.com>
2023-01-25 18:57:24 +0530

    avicii: Set the shipping API to 29 (Android 10)
    Change-Id: I0739afc923402038f9cff5b9b87accc8e98ebea2

--- device.mk
+++ device.mk
@@ -364,6 +364,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
+# Shipping API
+PRODUCT_SHIPPING_API_LEVEL := 29
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
