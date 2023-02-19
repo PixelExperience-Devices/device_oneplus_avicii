dkpost3/android_device_oneplus_avicii

avicii: Override available touchscreen gestures list
Change-Id: I14b0e034094c0cf4b60a86f5f26fc8d77ab50e43

touch/include/TouchscreenGestureConfig.h
@@ -1,0 +1,36 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "TouchscreenGesture.h"
+
+namespace vendor {
+namespace lineage {
+namespace touch {
+namespace V1_0 {
+namespace implementation {
+
+const int TouchscreenGesture::kSupportedGestures = makeBitField(
+        kGestureUpVee, kGestureLeftVee, kGestureRightVee, kGestureCircle, kGestureDoubleSwipe,
+        kGestureLeftToRight, kGestureRightToLeft, kGestureUpToDown, kGestureDownToUp, kGestureM,
+        kGestureW, kGestureSingleTap);
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace touch
+}  // namespace lineage
+}  // namespace vendor

PixelExperience-Devices/device_oneplus_avicii

touch/include/TouchscreenGestureConfig.h
@@ -1,0 +1,36 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "TouchscreenGesture.h"
+
+namespace vendor {
+namespace lineage {
+namespace touch {
+namespace V1_0 {
+namespace implementation {
+
+const int TouchscreenGesture::kSupportedGestures = makeBitField(
+        kGestureUpVee, kGestureLeftVee, kGestureRightVee, kGestureCircle, kGestureDoubleSwipe,
+        kGestureLeftToRight, kGestureRightToLeft, kGestureUpToDown, kGestureDownToUp, kGestureM,
+        kGestureW, kGestureS);
+
+}  // namespace implementation
+}  // namespace V1_0
+}  // namespace touch
+}  // namespace lineage
+}  // namespace vendor